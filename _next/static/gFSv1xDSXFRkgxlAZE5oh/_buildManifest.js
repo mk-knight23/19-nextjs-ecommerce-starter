575f102fa655cb9521c6c98f1165829748115441
mk.knight23 <mk.knight23@example.com>
2026-01-21 11:27:27 +0530

    Updates

---
+++ _next/static/gFSv1xDSXFRkgxlAZE5oh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/74-ee1d894775271753.js","static/chunks/pages/index-e3bfb5bfc64e73c5.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
